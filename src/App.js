601b204e00dcf016c28b0cbae16bbef2fb98395b
JacyDee274 <jacydee274@gmail.com>
2024-04-01 19:52:46 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import logo from "./5027435.png";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo img-fluid" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;

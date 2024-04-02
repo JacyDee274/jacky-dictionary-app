24a3e3fbe2150b32b5f4067a83556529e9cdb319
JacyDee274 <jacydee274@gmail.com>
2024-04-02 19:50:28 +0200

    Added search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
 import "./App.css";
 import logo from "./5027435.png";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>
+            Coded by{" "}
+            <a
+              href="https://github.com/JacyDee274"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Jacky Ilievski
+            </a>
+            , open-sourced on{" "}
+            <a
+              href="https://github.com/JacyDee274/jacky-dictionary-app"
+              target="_blank"
+              rel="noreferrer"
+            >
+              GitHub
+            </a>{" "}
+            and hosted on{" "}
+            <a
+              href="https://jacky-dictionary-app.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Netlify
+            </a>
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }

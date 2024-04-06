e8165640f53a704ce3043679a1f75bfaea07f3de
JacyDee274 <jacydee274@gmail.com>
2024-04-06 18:59:05 +0200

    Added synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,12 +1,23 @@
 import React from "react";
+import Synonyms from "./Synonyms.js";
 
 export default function Meaning(props) {
   console.log(props.meaning);
   return (
     <div className="Meaning">
-      <h3>{props.meaning.partOfSpeech}</h3>
-      <p>{props.meaning.definition}</p>
-      <p>{props.meaning.example}</p>
+      <h3>
+        <strong>Part of Speech: </strong>
+        {props.meaning.partOfSpeech}
+      </h3>
+      <p>
+        <strong>Definition: </strong>
+        {props.meaning.definition}
+      </p>
+      <p>
+        <strong>Example: </strong>
+        {props.meaning.example}
+      </p>
+      <Synonyms synonyms={props.meaning.synonyms} />
     </div>
   );
 }
